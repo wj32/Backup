b546802cfad30f7608a1b587e5c306fe513f6170
Wen Jia Liu <wj32.64@gmail.com>
2013-02-07 12:11:07 +1100

    added copyright notices

--- BackupExplorer/main.c
+++ BackupExplorer/main.c
@@ -1,3 +1,25 @@
+/*
+ * Backup Explorer -
+ *   startup
+ *
+ * Copyright (C) 2013 wj32
+ *
+ * This file is part of Backup.
+ *
+ * Backup is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Backup is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Backup.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "explorer.h"
 
 HINSTANCE BeInstanceHandle;
